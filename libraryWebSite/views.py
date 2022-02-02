36128ffc8a9c190f50fdb0f409ab8a34980b0f04
Programmingisfun11 <63208381+Programmingisfun11@users.noreply.github.com>
2022-02-02 17:37:13 +0100

    Update views.py

--- libraryWebSite/views.py
+++ libraryWebSite/views.py
@@ -7,7 +7,7 @@ from django.template import loader
 from django.shortcuts import render
 
 
-def index(request): #request musi bbyc bo do oznacza ze chcmey zadanie http
+def index(request): 
     bookSortByPrice=Book.objects.order_by('id')
     template=loader.get_template("index.html")
     contexOfPage={
@@ -18,7 +18,7 @@ def index(request): #request musi bbyc bo do oznacza ze chcmey zadanie http
 
 def showDetailBook(request, bookId):
     try:
-        #wez wszytskie dane ksiazk i o podanym id
+       
         question=Book.objects.get(pk=bookId)
         context={
             'detailOfBook':question
@@ -26,8 +26,6 @@ def showDetailBook(request, bookId):
        
     except Book.DoesNotExist: 
         raise Http404("book does not exists")
-        #context=context robimy tak gdy chcemy wsiazc np dane 
-        #jeszcze autora to wtedy musimy miec wiecej odniesien niz do jeden tabeli
     return render(request,"detailBook.html" ,context=context)
 
 
